8c851c8edd6f6543b284c57a17b2a5059fe2fafb
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2023-02-12 20:13:40 -0600

    Initial commit

---
+++ Calculadora.cpp
@@ -0,0 +1,52 @@
+#include "Calculadora.hpp"
+
+//Constructor por default
+Calculadora::Calculadora()
+{
+    
+}
+
+//Constructor con parametros
+Calculadora::Calculadora(double x, double y) {
+    
+}
+
+//Establecer el valor de x
+void Calculadora::setX(double x) {
+    
+}
+
+//Establecer el valor de y
+void Calculadora::setY(double y) {
+   
+}
+
+//Obtener el valor de x
+double Calculadora::getX() {
+    return 0;
+}
+
+//Obtener el valor de y
+double Calculadora::getY() {
+    return 0;
+}
+
+//La suma de dos numeros
+double Calculadora::suma() {
+    return 0;
+}
+
+//La multiplicacion de dos numeros
+double Calculadora::multiplica() {
+    return 0;
+}
+
+//La resta de dos numeros
+double Calculadora::resta() {
+    return 0;
+}
+
+//La division de dos numeros
+double Calculadora::divide() {
+    return 0;
+}
